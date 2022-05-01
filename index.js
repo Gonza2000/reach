28c689482b9b970f9f0f61c9691c9b375b6792dd
Gonzalo Cárdenas <gonza.card2000@hotmail.com>
2022-05-01 12:55:42 -0500

    why reach

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const navToggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".nav_links");
 navToggle.addEventListener("click", () => {
+
     navMenu.classList.toggle("nav_links_visible")
+
 })
+if (state == true) {
+    document.querySelector(".nav_links").classList.remove("nav_links_visible");
+}
