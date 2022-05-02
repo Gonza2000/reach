ad8c13a5dcbfa325c7bf109c9673623673dfc099
Gonzalo Cárdenas <gonza.card2000@hotmail.com>
2022-05-02 13:11:25 -0500

    nav menu last change

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const navToggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".nav_links");
-navToggle.addEventListener("click", () => {
+const item = document.getElementById("servicio");
+const items = document.getElementById("servicios");
 
+navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav_links_visible")
 
 })
-if (state == true) {
-    document.querySelector(".nav_links").classList.remove("nav_links_visible");
-}
+item.addEventListener("click", () => {
+    navMenu.classList.remove("nav_links_visible");
+})
+items.addEventListener("click", () => {
+    navMenu.classList.remove("nav_links_visible");
+})
